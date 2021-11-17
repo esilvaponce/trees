25504a55c00870e3d2ad6c2e1818b08c841255ee
Enrique Silva <esilvaponce@gmail.com>
2021-11-17 02:41:12 -0600

    Tree, reading JSON

--- src/tree/TreeNode.js
+++ src/tree/TreeNode.js
@@ -1,12 +1,24 @@
-const TreeNode = ({ node, children }) => {
+const TreeNode = ({ node, children, level }) => {
+  
+  const defineClassName = (level) => {
+    console.log(level);
+    if(level.split('.').length === 2) {
+      return "tab";
+    } else if (level.split('.').length === 3) {
+      return "tab2";
+    } else if (level.split('.').length === 4) {
+      return "tab3";
+    } else {
+      return "";
+    }
+  }
+
   return (
     <>
-      {node}
+      <p className={defineClassName(level)}>{node}</p>
       {children &&
         children.map((node) => (
-          <div key={node.node} className="indent">
             <TreeNode {...node} />
-          </div>
         ))}
     </>
   );
--- src/tree/index.js
+++ src/tree/index.js
@@ -1,15 +1,10 @@
 import "./index.css";
+import dataJSON from "./data.json";
+import TreeNode from './TreeNode';
 
 export default function Tree() {
+  
   return (
-    <div className="tree">
-      <p>1 root</p>
-      <p className="tab">1.1 ant</p>
-      <p className="tab">1.2 bear</p>
-      <p className="tab2">1.2.1 cat</p>
-      <p className="tab2">1.2.2 dog</p>
-      <p className="tab3">1.2.2.1 elephant</p>
-      <p className="tab">1.3 frog</p>
-    </div>
+    <TreeNode {...dataJSON}/>
   );
 }
